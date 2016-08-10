30e4e350be2e67485ce116f4533863625eb96788
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-09 21:03:43 -0500

    removed zip folder

---
+++ ADGP105/ADGP105/WumpusShooter/WumpusShooter/Pit.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream> //c++ lib.
+#include <string> //c++ lib.
+#include "Cell.h" //header file
+
+//Pit struct: Used to set the position for pit locations (public)
+struct Pit //pit struct
+{
+	Cell pPosition; //used for grid position
+};
---
+++ ADGP105/ADGP105/WumpusShooter/WumpusShooter/Shoot.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream> //c++ lib.
+#include <string> //c++ lib.
+#include "Cell.h" //header file
+
+//Shoot struct: Used to set the position for shoot locations (public)
+struct Shoot //shoot struct
+{
+	Cell aPosition; //used for grid position
+};
